ec286000372abe5bb5f4aaf07cc039794e6fd707
Kumar Gaurav <kgthegreat@gmail.com>
2015-06-15 07:34:07 +0530

    Writing to db

--- whatbook.go
+++ whatbook.go
@@ -4,21 +4,82 @@ import (
 	"log"
 	"net/http"
 	"html/template"
+	"fmt"
+	r "github.com/dancannon/gorethink"
 )
 
+
+
+var session *r.Session
+
+type Answer struct {
+    Id    string `gorethink:"id,omitempty"`
+    Book_Id  string `gorethink:"book_id"`
+    Preference string `gorethink:"preference"`
+}
+
+func init() {
+    var err error
+    session, err = r.Connect(r.ConnectOpts{
+        Address:  "localhost:28015",
+        Database: "whatbook",
+    })
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+}
+
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index")
+	renderTemplate(w, "index", map[string]interface{}{
+		"ImageUrl": "/images/coetzee.jpg",
+		"Title": "Youth",
+		"Author": "J.M. Coetzee",
+	})
+
 } 
 
+func answerHandler(w http.ResponseWriter, res *http.Request) {
+	var preference string
+	if res.FormValue("yes") == "on" {
+		preference = "yes"
+		fmt.Println("In if")
+	} else if res.FormValue("neutral") == "on" {
+		preference = "neutral"
+	} else if res.FormValue("no") == "on" {
+		preference = "no"
+	}
+
+	var data = map[string]interface{}{
+		"book_id": "1",
+		"preference": preference,
+	}
+
+	result, err := r.Table("answers").Insert(data).RunWrite(session)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("*** Insert result: ***")
+	fmt.Println("*** Insert result: ** %v", result)
+
+	renderTemplate(w, "index", map[string]interface{}{
+		"ImageUrl": "/images/windup-bird.jpg",
+		"Title": "The Wind Up Bird Chronicles",
+		"Author": "Haruki Murakami",
+	})
+
+}
 
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-    t, err := template.ParseFiles(tmpl + ".html")
+func renderTemplate(w http.ResponseWriter, tmpl string, vars interface{}) {
+	t, err := template.ParseFiles(tmpl + ".html")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-	err = t.Execute(w, nil)
+	err = t.Execute(w, vars)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
@@ -36,5 +97,6 @@ func main() {
 	http.Handle("/images/", http.StripPrefix("/images/", imagesHandler))
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/answer", answerHandler)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
